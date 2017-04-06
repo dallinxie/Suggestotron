952cc025556e743b08fdc26c1c4e2ec20543ceea
dallin <dallin@dallinxiedeMac.local>
2017-04-06 22:51:14 +0800

    second step

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root "topics#index"
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
